176cbf62363795c0e85d01904b6ecd58a35c9197
renzodamian <renzojaramillocueva@gmail.com>
2023-01-12 12:28:34 -0500

    update eureka

--- user-service/src/main/java/com/userservice/service/UserService.java
+++ user-service/src/main/java/com/userservice/service/UserService.java
@@ -40,12 +40,12 @@ public class UserService {
     }
     //Get RestTemplate
     public List<Car> getCars (int userId) {
-        List<Car>   cars =  restTemplate.getForObject("http://localhost:8002/car/user/"+userId, List.class);
+        List<Car>   cars =  restTemplate.getForObject("http://car-service/car/user/"+userId, List.class);
         return cars;
     }
     //Get RestTemplate
     public List<Bike> getBikes (int userId) {
-        List<Bike>   bikes =  restTemplate.getForObject("http://localhost:8003/bike/user/"+userId, List.class);
+        List<Bike>   bikes =  restTemplate.getForObject("http://bike-service/bike/user/"+userId, List.class);
         return bikes;
     }
     //Post FeignClient
